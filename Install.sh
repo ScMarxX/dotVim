bb4f826bd4440f7a5079ddff23a27af0194be1b4
ScMarxX <ScMarxX@gmail.com>
2013-05-25 12:13:18 +0800

    update Install.sh. fonts should be save at ~/.fonts, not ~/font.

--- Install.sh
+++ Install.sh
@@ -1,27 +1,45 @@
 #!/bin/bash
-echo git submodule init
+echo =======================================
+echo "Install Vundle and fonts"
+echo "git submodule init"
 git submodule init
-echo git submodule update
+echo "git submodule update"
 git submodule update
 
+echo "======================================="
+echo "Link to ~/.vim ~/.vimrc && ~/.fonts"
 echo "pwd:$PWD"
-echo mv ~/.vim{,.backup}
+
+echo "mv ~/.vim{,.backup}"
 mv ~/.vim{,.backup}
-echo ln -s $PWD ~/.vim
+echo "ln -s $PWD ~/.vim"
 ln -s $PWD ~/.vim
-echo mv ~/.font{,.backup}
-mv ~/.font{,.backup}
-echo ln -s ~/.vim/font ~/.font
-ln -s ~/.vim/font ~/.font
-echo mv ~/.vimrc{,.backup}
+
+echo "mv ~/.fonts{,.backup}"
+mv ~/.fonts{,.backup}
+echo "ln -s ~/.vim/font ~/.fonts"
+ln -s ~/.vim/font ~/.fonts
+
+echo "mv ~/.vimrc{,.backup}"
 mv ~/.vimrc{,.backup}
-echo ln -s ~/.vim/vimrc ~/.vimrc
+echo "ln -s ~/.vim/vimrc ~/.vimrc"
 ln -s ~/.vim/vimrc ~/.vimrc
 
-echo ls -lah ~/.vim ~/.vimrc ~/.font
-ls -lah ~/.vim ~/.vimrc ~/.font
+echo =======================================
+echo "ls -lah ~/.vim ~/.vimrc ~/.font"
+ls -lah ~/.vim ~/.vimrc ~/.fonts
 
-echo cd ~/.font
-cd ~/.font
-echo sudo fc-cache -vf
+echo =======================================
+echo "update fonts"
+echo "sudo fc-cache -vf"
 sudo fc-cache -vf
+
+echo =======================================
+echo "Installing ctags && cscope"
+sudo apt-get install -y ctags cscope
+
+echo =======================================
+echo "Installing other plugins"
+vi < RunVundleInstall.txt
+echo =======================================
+
